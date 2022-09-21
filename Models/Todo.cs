05a233393ba035f124c5caaa7500b16a4a8ec5e5
Marco Jankovic <Marcojankovice@hotmail.com>
2022-09-21 11:11:30 +0200

    Newest

---
+++ Models/Todo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Todo.Models
+{
+    internal class Todo
+    {
+        private readonly int Id;
+        private string? Description;
+        private bool Done;
+
+
+        public Todo(int id, string description)
+        {
+            Id = id;
+            Description = description;          
+        }
+
+        public int id
+        {
+            get { return Id; }
+
+        }
+
+        public string? description
+        {
+            get { return Description; }
+            set { Description = value; }
+        }
+
+
+
+    }
+}
